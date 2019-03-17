c34de68996dbba3ba32ae1f359c671c1bc289391
craig wadding <craig.wadding@gmail.com>
2019-03-17 18:04:55 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -20,11 +20,12 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+  animals = [ "dog", "cat", "mouse", "cow" ]
   animals#pop
 end
 
 def remove_element_from_start_of_array(array)
+  
   animals#unshift
   
 end
