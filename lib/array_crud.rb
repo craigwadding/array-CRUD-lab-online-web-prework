86edbca47bcf523a914b66109f644a1890fb5d84
craig wadding <craig.wadding@gmail.com>
2019-03-17 18:05:05 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -31,13 +31,14 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  animals = [ "dog", "cat", "mouse", "cow" ]
   animals[2]
   
   
 end
 
 def retrieve_first_element_from_array(array)
+  
   animals[0]
   
 end
